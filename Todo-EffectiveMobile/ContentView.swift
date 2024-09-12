e15dfe9c2362658623fc8df2d48b33b86a67a1d5
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-09-12 14:55:35 +0200

    new project

---
+++ Todo-EffectiveMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo-EffectiveMobile
+//
+//  Created by Dmitrii Grigorev on 12.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-EffectiveMobile/Todo_EffectiveMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_EffectiveMobileApp.swift
+//  Todo-EffectiveMobile
+//
+//  Created by Dmitrii Grigorev on 12.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_EffectiveMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
