f82238332f1f8feeb70dd560dc20d4ebf136351e
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-09-13 13:05:03 +0200

    - networking: moved folder - viewData: added TodoItemViewData

---
+++ Todo-EffectiveMobile/model/viewData/TodoItemViewData.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListViewData.swift
+//  Todo-EffectiveMobile
+//
+//  Created by Dmitrii Grigorev on 13.09.24.
+//
+
+import Foundation
+
+struct TodoItemViewData : Hashable {
+    let title : String
+    let description : String
+    let creationDate : Date?
+    let todoDateStart : Date?
+    let todoDateEnd : Date?
+    let isCompleted : Bool
+}
