d2e44ae03463bededb32194754ccc6fea1e3ab88
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-09-15 20:32:52 +0200

    view: added TodoItemCellView viewData: added inits

--- Todo-EffectiveMobile/model/viewData/TodoItemViewData.swift
+++ Todo-EffectiveMobile/model/viewData/TodoItemViewData.swift
@@ -8,11 +8,33 @@
 import Foundation
 
 struct TodoItemViewData : Hashable {
-    let id : Int
+    let id : UUID
     let title : String
     let description : String
     let creationDate : Date
     let todoDateStart : Date?
     let todoDateEnd : Date?
     let isCompleted : Bool
+    
+    init(id : UUID, title: String, description: String, creationDate: Date, todoDateStart: Date?, todoDateEnd: Date?, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.creationDate = creationDate
+        self.todoDateStart = todoDateStart
+        self.todoDateEnd = todoDateEnd
+        self.isCompleted = isCompleted
+    }
+}
+
+extension TodoItemViewData : Identifiable {
+    init(old : Self,isCompleted: Bool) {
+        self.id = old.id
+        self.title = old.title
+        self.description = old.description
+        self.creationDate = old.creationDate
+        self.todoDateStart = old.todoDateStart
+        self.todoDateEnd = old.todoDateEnd
+        self.isCompleted = isCompleted
+    }
 }
---
+++ Todo-EffectiveMobile/view/TodoItemCellView.swift
@@ -0,0 +1,8 @@
+//
+//  TodoItemCellView.swift
+//  Todo-EffectiveMobile
+//
+//  Created by Dmitrii Grigorev on 15.09.24.
+//
+
+import Foundation
